f9d113747153c4683718630790eb4dbf364b0014
chirayu16 <chirayupo16@gmail.com>
2025-03-12 12:40:14 +0530

    array operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArrayObjectsComponent } from './array-objects/array-objects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArrayObjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
